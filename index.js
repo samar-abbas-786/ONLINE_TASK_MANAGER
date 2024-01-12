0029ee11419a1e496fede350a75a84a7ecbe44e8
mohammad ali <mohammadali200314@gmail.com>
2024-01-12 07:10:44 +0530

    fixed form signup issue

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const User = require("./models/taskSchema"); // Import the User model with corre
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
 
 // Middleware to parse incoming JSON data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // DATABASE CONNECTION
 mongoose
-  .connect("mongodb://localhost:27017/task-manager", {})
+  .connect("mongodb://127.0.0.1:27017/task-manager")
   .then(() => {
     console.log("MongoDB Connected");
   })
@@ -40,8 +42,8 @@ app.get("/", (req, res) => {
 // });
 // Route to handle the signup form submission
 app.post("/user/signup", async (req, res) => {
-  console.log(req.body);
   const { fullName, email, password } = req.body;
+  console.log(req.body);
   try {
     await User.create({
       fullName,
