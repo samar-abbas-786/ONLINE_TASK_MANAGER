c2f8584fa6dbc3fa4c54b7b7a893b35f6ec83d58
Samar Abbas <samarabbas172003@gmail.com>
2024-01-12 08:36:41 +0530

    Second  Commit

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const User = require("./models/taskSchema"); // Import the User model with corre
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
 
 // Middleware to parse incoming JSON data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // DATABASE CONNECTION
 mongoose
-  .connect("mongodb://localhost:27017/task-manager", {})
+  .connect("mongodb://127.0.0.1:27017/task-manager")
   .then(() => {
     console.log("MongoDB Connected");
   })
@@ -28,6 +30,9 @@ app.set("views", path.resolve(__dirname, "views")); // Correct path to the views
 app.get("/", (req, res) => {
   return res.render("signup");
 });
+app.get("/login", (req, res) => {
+  return res.render("login");
+});
 
 // app.post("/user/signup", async (req, res) => {
 //   const data = {
@@ -40,19 +45,34 @@ app.get("/", (req, res) => {
 // });
 // Route to handle the signup form submission
 app.post("/user/signup", async (req, res) => {
-  console.log(req.body);
   const { fullName, email, password } = req.body;
+  console.log(req.body);
   try {
     await User.create({
       fullName,
       email,
       password,
     });
-    res.status(201).json({ message: "User created successfully!" });
+    // res.status(201).json({ message: "User created successfully!" });
+    return res.redirect("/home");
   } catch (error) {
     console.error("Error creating user:", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
+});
+
+app.post("/user/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email: email, password: password });
+  if (!user) {
+    res.render("signup");
+  } else {
+    return res.redirect("/home");
+  }
+});
+
+app.get("/home", (req, res) => {
+  return res.render("task");
 });
 
 // Start the server
